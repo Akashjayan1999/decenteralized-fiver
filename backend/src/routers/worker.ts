0cc6932c84b1c05ec84c651e127107e074e830cc
Akashjayan1999 <akashjayan999@gmail.com>
2025-07-16 07:26:08 +0000

    add user forntent

--- backend/src/routers/worker.ts
+++ backend/src/routers/worker.ts
@@ -34,6 +34,88 @@ router.post("/signin", async (req, res) => {
     }
 });
 
+router.post("/payout", workerMiddleware, async (req, res) => {
+    // @ts-ignore
+    const userId: string = req.userId;
+    const worker = await prismaClient.worker.findFirst({
+        where: { id: Number(userId) }
+    })
+
+    if (!worker) {
+         res.status(403).json({
+            message: "User not found"
+        })
+        return;
+    }
+
+    const transaction = new Transaction().add(
+        SystemProgram.transfer({
+            fromPubkey: new PublicKey("2KeovpYvrgpziaDsq8nbNMP4mc48VNBVXb5arbqrg9Cq"),
+            toPubkey: new PublicKey(worker.address),
+            lamports: 1000_000_000 * worker.pending_amount / TOTAL_DECIMALS,
+        })
+    );
+
+
+    console.log(worker.address);
+
+    const keypair = Keypair.fromSecretKey(decode(privateKey));
+
+    // TODO: There's a double spending problem here
+    // The user can request the withdrawal multiple times
+    // Can u figure out a way to fix it?
+    let signature = "";
+    try {
+        signature = await sendAndConfirmTransaction(
+            connection,
+            transaction,
+            [keypair],
+        );
+    
+     } catch(e) {
+         res.json({
+            message: "Transaction failed"
+        })
+        return;
+     }
+    
+    console.log(signature)
+
+    // We should add a lock here
+    await prismaClient.$transaction(async tx => {
+        await tx.worker.update({
+            where: {
+                id: Number(userId)
+            },
+            data: {
+                pending_amount: {
+                    decrement: worker.pending_amount
+                },
+                locked_amount: {
+                    increment: worker.pending_amount
+                }
+            }
+        })
+
+        await tx.payouts.create({
+            data: {
+                user_id: Number(userId),
+                amount: worker.pending_amount,
+                status: "Processing",
+                signature: signature
+            }
+        })
+    })
+
+    res.json({
+        message: "Processing payout",
+        amount: worker.pending_amount
+    })
+
+
+})
+
+
 router.get("/balance", workerMiddleware, async (req, res) => {
     // @ts-ignore
     const userId: string = req.userId;
